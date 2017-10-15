ce5a85988893b5c3b6e30dbb6fccd478ce0f4b7b
arg0n1s <basti_seppl@yahoo.com>
2017-10-15 14:48:20 +0200

    Vers: 0.1.0
    - odometry is working correctly
    - did some major refactoring

--- include/pses_odometry/OdometryHelper.h
+++ include/pses_odometry/OdometryHelper.h
@@ -5,178 +5,73 @@
 #include <geometry_msgs/Point.h>
 #include <pses_odometry/ForwardKinematics.h>
 #include <sensor_msgs/Imu.h>
+#include <tf/transform_broadcaster.h>
 
 #define WHEEL_RADIUS 0.032
-#define RAD_PER_TICK 0.7853981634 // 2*PI/8
+#define RAD_PER_TICK 0.7853981634             // 2*PI/8
 #define DRIVEN_DISTANCE_PER_TICK 0.0251327412 // RAD_PER_TICK * WHEEL_RADIUS
-#define STANDARD_GRAVITY 9.80665 // m/s^2  
+#define STANDARD_GRAVITY 9.80665              // m/s^2
 
-class OdometryHelper{
+class OdometryHelper
+{
 public:
-	OdometryHelper() {
-		yaw = 0.0;
-		pitch = 0.0;
-		roll = 0.0;
-		dt = 0.0;
-		drivenDistance = 0.0;
-		speed = 0.0;
-		drivingDirection = 0;
-        prevDirection = 0;
-        wxOffset = 0.0;
-        wyOffset = 0.0;
-        wzOffset = 0.0;
-        axOffset = 0.0;
-        ayOffset = 0.0;
-        azOffset = 0.0;
-        dataCount = 0;
-        imuCalibrated = false;
-		oldTimeStamp = ros::Time::now();
-		odometric.setK(0.25); // set distance between front axis and back axis (in meters)
-	}
+  OdometryHelper();
+  void updateSensorData(const sensor_msgs::Imu& imuData, const double hallDt,
+                        const int motor);
+  void updateMotorLevel(const int motorLevel);
+  const double& getYaw() const;
+  const double& getRoll() const;
+  const double& getPitch() const;
+  const double& getSpeed() const;
+  double getVx() const;
+  double getVy() const;
+  const double& getWz() const ;
+  void getQuaternion(geometry_msgs::Quaternion& quat) const;
+  const double& getDrivenDistance() const;
+  const geometry_msgs::Point& getPosition() const;
+  const bool& isImuCalibrated() const;
 
-  inline void updateSensorData(const sensor_msgs::Imu& imuData, const double hallDt) {
-    calcDt(imuData.header.stamp, oldTimeStamp);
-		if(imuCalibrated) {
-      this->imuData.angular_velocity.x = imuData.angular_velocity.x - wxOffset;
-      this->imuData.angular_velocity.y = imuData.angular_velocity.y - wyOffset;
-      this->imuData.angular_velocity.z = imuData.angular_velocity.z - wzOffset;
-      this->imuData.linear_acceleration.x = imuData.linear_acceleration.x - axOffset;
-      this->imuData.linear_acceleration.y = imuData.linear_acceleration.y - ayOffset;
-      this->imuData.linear_acceleration.z = imuData.linear_acceleration.z - azOffset;
-      yaw = integrateEulerAngles(yaw, this->imuData.angular_velocity.z, dt);
-      pitch = integrateEulerAngles(pitch, this->imuData.angular_velocity.y, dt);
-      roll = integrateEulerAngles(roll, this->imuData.angular_velocity.x, dt);
-		}
-    else calibrateIMU();
-    this->hallDt = hallDt;
-    oldTimeStamp = imuData.header.stamp;
-		calcSpeed();
-		calcDeltaDistance();
-		calcDrivenDistance();
-		calcPosition();
-	}
-
-
-  inline void updateMotorLevel(const int motorLevel) {
-    this->motorLevel = motorLevel;
-        prevDirection = drivingDirection;
-        if(motorLevel < 0) drivingDirection = -1;
-    else if(motorLevel > 0) drivingDirection = 1;
-		else drivingDirection = 0;
-	}
-
-	inline double getYaw() {
-		return yaw;
-	}
-	inline double getRoll() {
-		return roll;
-	}
-	inline double getPitch() {
-		return pitch;
-	}
-	inline float getSpeed() {
-		return speed;
-	}
-	inline void getQuaternion(geometry_msgs::Quaternion& quat) {
-		quat = tf::createQuaternionMsgFromYaw(yaw);
-	}
-	inline float getDrivenDistance() {
-		return drivenDistance;
-	}
-	inline geometry_msgs::Point getPosition() {
-		return position;
-	}
-	inline bool isImuCalibrated() {
-		return imuCalibrated;
-	}
 private:
-	double yaw;
-	double roll;
-	double pitch;
-	double dt;
-	float speed;
-	float drivenDistance;
-	float deltaDistance;
-    int prevDirection;
-	int drivingDirection; // -1 = backwards; 0 = stop; 1 = forwards
-	
-	//IMU offsets
-	double wxOffset;
-	double wyOffset;
-	double wzOffset;
-	double axOffset;
-	double ayOffset;
-	double azOffset;
-
-	uint32_t dataCount;
-	bool imuCalibrated;
-	ros::Time oldTimeStamp;
-	geometry_msgs::Point position;
-  ForwardKinematics odometric;
-  int motorLevel;
+  // sensor data
   double hallDt;
   sensor_msgs::Imu imuData;
-	inline void calcDt(const ros::Time& currentTimeStamp, const ros::Time& oldTimeStamp) {
-		dt = (currentTimeStamp - oldTimeStamp).toSec();
-	}
+  // command data
+  int motorLevel;
+  int prevDirection;
+  int drivingDirection; // -1 = backwards; 0 = stop; 1 = forwards
 
-	inline void calibrateIMU() {
-    if(dataCount < 500) {
-			dataCount++;
-      wxOffset += imuData.angular_velocity.x;
-      wyOffset += imuData.angular_velocity.y;
-      wzOffset += imuData.angular_velocity.z;
-      axOffset += imuData.linear_acceleration.x;
-      ayOffset += imuData.linear_acceleration.x;
-      azOffset += (imuData.linear_acceleration.z - STANDARD_GRAVITY);
-		}
-		else if(dataCount == 500 && !imuCalibrated) {
-			wxOffset /= dataCount;
-			wyOffset /= dataCount;
-			wzOffset /= dataCount;
-			axOffset /= dataCount;
-			ayOffset /= dataCount;
-			azOffset /= dataCount;
-			imuCalibrated = true;
-		}
-	}
+  // calculated values
+  double yaw;
+  double roll;
+  double pitch;
+  double dt;
+  double speed;
+  double drivenDistance;
+  double deltaDistance;
 
-	inline void calcRPY(){
-	}
-	inline double integrateEulerAngles(double angle, double dAngle, double dT){
-		double result = angle + dAngle*dT;
-			if(result>M_PI){
-				return -2*M_PI+result;
-			}else if(result<-M_PI){
-				return 2*M_PI+result;
-			}else{
-				return result;
-			}
-	}
+  // IMU offsets
+  double wxOffset;
+  double wyOffset;
+  double wzOffset;
+  double axOffset;
+  double ayOffset;
+  double azOffset;
 
-	inline void calcSpeed() {
-        if(!std::isnan(hallDt)){
-            speed =  drivingDirection * DRIVEN_DISTANCE_PER_TICK / hallDt;
-        }else{
-            if(prevDirection!=drivingDirection) speed = 0;
-            else if(drivingDirection == 0) speed =  0;
-        }
-	}
+  // state variables
+  uint32_t dataCount;
+  bool imuCalibrated;
+  ros::Time oldTimeStamp;
+  geometry_msgs::Point position;
+  ForwardKinematics odometric;
 
-
-	inline void calcDeltaDistance() {
-    deltaDistance = std::isnan(hallDt)?0.0 : drivingDirection*DRIVEN_DISTANCE_PER_TICK;
-	}
-
-	inline void calcDrivenDistance() {
-		drivenDistance = drivenDistance + deltaDistance;
-	}
-	inline void calcPosition() {
-		std::vector<double> pos = odometric.getUpdateWithGyro(yaw, deltaDistance);
-		position.x = pos.at(1);
-		position.y = -pos.at(0);
-		position.z = 0.0;
-	}
+  void calcDt(const ros::Time& currentTimeStamp, const ros::Time& oldTimeStamp);
+  void calibrateIMU();
+  void calcRPY();
+  void integrateEulerAngles(const double& dAngle, double& angle);
+  void calcSpeed();
+  void calcDeltaDistance();
+  void calcDrivenDistance();
+  void calcPosition();
 };
 
 #endif
--- src/odometry_node.cpp
+++ src/odometry_node.cpp
@@ -8,102 +8,6 @@
 #include <std_msgs/Float64.h>
 #include <std_msgs/UInt8.h>
 
-/*
-void buildOdometryTransformation(geometry_msgs::TransformStamped& odomTransform,
-                                 const pses_basis::SensorData& sensorData,
-                                 OdometryHelper& odomHelper,
-                                 geometry_msgs::Quaternion& odomQuaternion)
-{
-
-  odomTransform.header.stamp = sensorData.header.stamp;
-  odomTransform.header.frame_id = "odom";
-  odomTransform.child_frame_id = "base_footprint";
-  geometry_msgs::Point position = odomHelper.getPosition();
-  odomTransform.transform.translation.x = position.x;
-  odomTransform.transform.translation.y = position.y;
-  odomTransform.transform.translation.z = 0.0;
-  odomTransform.transform.rotation = odomQuaternion;
-}
-*/
-/*
-void buildOdometryMessage(nav_msgs::Odometry& odom,
-                          const pses_basis::SensorData& sensorData,
-                          OdometryHelper& odomHelper,
-                          geometry_msgs::Quaternion& odomQuaternion)
-{
-
-  odom.header.stamp = sensorData.header.stamp;
-  odom.header.frame_id = "odom";
-
-  // set the position
-  odom.pose.pose.position = odomHelper.getPosition();
-  odom.pose.pose.orientation = odomQuaternion;
-
-  // set the velocity
-  odom.child_frame_id = "base_footprint";
-  odom.twist.twist.linear.x =
-      odomHelper.getSpeed() * std::cos(sensorData.angular_velocity_z);
-  odom.twist.twist.linear.y =
-      odomHelper.getSpeed() * std::sin(sensorData.angular_velocity_z);
-  odom.twist.twist.angular.z = sensorData.angular_velocity_z;
-}
-*/
-/*
-void buildInfoMessage(pses_basis::CarInfo& info,
-                      const pses_basis::SensorData& sensorData,
-                      OdometryHelper& odomHelper)
-{
-  // configure header
-  info.header.stamp = sensorData.header.stamp;
-  info.header.frame_id = sensorData.header.frame_id;
-  info.header.seq++;
-  info.header.car_id = sensorData.header.car_id;
-  // set rpy-Angles
-  info.roll = odomHelper.getRoll();
-  info.pitch = odomHelper.getPitch();
-  info.yaw = odomHelper.getYaw();
-  // set driven distance
-  info.driven_distance = odomHelper.getDrivenDistance();
-  // set speed
-  info.speed = odomHelper.getSpeed();
-}
-
-void commandCallback(const pses_basis::Command::ConstPtr& cmd,
-                     OdometryHelper* odomHelper)
-{
-  // update the command information for later odometric calculations
-  odomHelper->updateCommand(*cmd);
-}
-*/
-/*
-void dataCallback(const pses_basis::SensorData::ConstPtr& sensorData,
-                  tf::TransformBroadcaster* odomBroadcaster,
-                  ros::Publisher* odom_pub, OdometryHelper* odomHelper)
-{
-
-  pses_basis::SensorData sensorDataCopy = *sensorData;
-  // update sensor data for later odometric calculations
-  odomHelper->updateSensorData(sensorDataCopy);
-  // objects to store the odometry and its transform
-  geometry_msgs::TransformStamped odomTransform;
-  nav_msgs::Odometry odom;
-  pses_basis::CarInfo info;
-  // since all odometry is 6DOF we'll need a quaternion created from yaw
-  geometry_msgs::Quaternion odomQuaternion;
-  odomHelper->getQuaternion(odomQuaternion);
-  // build odom transform for tf and the odometry message
-  buildOdometryTransformation(odomTransform, sensorDataCopy, *odomHelper,
-                              odomQuaternion);
-  buildOdometryMessage(odom, sensorDataCopy, *odomHelper, odomQuaternion);
-  buildInfoMessage(info, sensorDataCopy, *odomHelper);
-  // send the transform
-  odomBroadcaster->sendTransform(odomTransform);
-  // publish the odometry message
-  odom_pub->publish(odom);
-  carInfo_pub->publish(info);
-}
-*/
-
 void imuCallback(sensor_msgs::Imu::ConstPtr msg, sensor_msgs::Imu* out){
   *out = *msg;
 }
@@ -150,8 +54,27 @@ int main(int argc, char** argv)
   ros::Rate loop_rate(150);
   while (ros::ok())
   {
-    //odomHelper->updateSensorData(sensorDataCopy);
+    odomHelper.updateSensorData(imu, hallDt.data, motor.data);
+    odomHelper.getQuaternion(odomQuaternion);
 
+    // build odometry transform
+    odomTransform.header.stamp = ros::Time::now();
+    odomTransform.header.frame_id = "odom";
+    odomTransform.child_frame_id = "base_footprint";
+    odomTransform.transform.translation.x = odomHelper.getPosition().x;
+    odomTransform.transform.translation.y = odomHelper.getPosition().y;
+    odomTransform.transform.translation.z = 0.0;
+    odomTransform.transform.rotation = odomQuaternion;
+
+    // build odometry message
+    odom.header.stamp = ros::Time::now();
+    odom.header.frame_id = "odom";
+    odom.pose.pose.position = odomHelper.getPosition();
+    odom.pose.pose.orientation = odomQuaternion;
+    odom.child_frame_id = "base_footprint";
+    odom.twist.twist.linear.x = odomHelper.getVx();
+    odom.twist.twist.linear.y = odomHelper.getVy();
+    odom.twist.twist.angular.z = odomHelper.getWz();
 
     odomPub.publish(odom);
     odomBroadcaster.sendTransform(odomTransform);
